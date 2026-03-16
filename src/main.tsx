a9dee9d3a3a7350c3d67c4ffa96b605689969363
lucassrblt <lrimbault92@gmail.com>
2026-03-16 13:54:43 +0100

    feat(style): add Google Fonts and theme configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
